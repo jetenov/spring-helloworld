15498d813d60533764eebbf7a50bbb3da4f7576d
aibatjetenov <jetenov97@mail.ru>
2021-01-13 00:26:04 +0600

    13/01/2021 AJetenov - 2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,12 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.equals(bean1));
+
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat1.equals(beanCat2));
     }
 }
